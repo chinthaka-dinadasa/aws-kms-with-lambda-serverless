999792d54e54d62ce0fa401d576c6271f5e5d9ea
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2025-01-11 16:42:05 +0530

    added prettier

--- handler.ts
+++ handler.ts
@@ -1,8 +1,18 @@
-exports.hello = async (event) => {
-  return {
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+interface Response {
+  statusCode: number;
+  body: string;
+}
+
+exports.hello = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const response: Response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
+      message: 'Go Serverless v4! Your function executed successfully!',
     }),
   };
+  return response;
 };
