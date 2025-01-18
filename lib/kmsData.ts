14191b65b5a0ca64ec8d55367bcb2ccad0358fef
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2025-01-19 00:52:51 +0530

    code aded

--- lib/kmsData.ts
+++ lib/kmsData.ts
@@ -7,6 +7,8 @@ import {
 const kmsClient = new KMSClient({ region: process.env.AWS_REGION });
 
 export const processDecrytion = async (encryptedData: string) => {
+
+  //Decryption using AWS KMS CLient
   const decryptInput: DecryptCommandInput = {
     CiphertextBlob: Buffer.from(encryptedData, 'base64'),
     KeyId: `${process.env.APP_CONFIG_KMS_KEY_ARN}`,
@@ -17,6 +19,7 @@ export const processDecrytion = async (encryptedData: string) => {
   const decryptionResponse = await kmsClient.send(decryptCommand);
   const decryptedText = decryptionResponse.Plaintext;
 
+  //Decoding the decrypted text using text decoder to get the original text
   const decoder = new TextDecoder("utf-8");
   const plaintextString = decoder.decode(decryptedText);
   console.log('decryptedText', plaintextString);
